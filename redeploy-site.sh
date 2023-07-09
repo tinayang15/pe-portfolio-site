26ba5a017c65767f5c66033966880d1fcca6d817
Tina Yang <Tinayangers@gmail.com>
2023-07-09 01:09:35 -0400

    Added redeploy-site sh

---
+++ redeploy-site.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+tmux kill-session -a
+
+cd pe-portfolio-site
+
+git fetch && git reset origin/main --hard
+
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+
+tmux new-session -d -s flask-server
+tmux send-keys "source python3-virtualenv/bin/activate" C-m
+tmux send-keys "flask run --host=0.0.0.0" C-m
