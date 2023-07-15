740935c109516a08172681102312211c0f319e3d
Tina Yang <Tinayangers@gmail.com>
2023-07-15 15:42:37 -0400

    updated redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-tmux kill-session -a
-
-cd /Users/tinayang/MLH/Portfolio/pe-portfolio-site
+cd /root/pe-portfolio-site
 
 git fetch && git reset origin/main --hard
 
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux new-session -d -s flask-server
-tmux send-keys "source python3-virtualenv/bin/activate" C-m
-tmux send-keys "flask run --host=0.0.0.0" C-m
+systemctl restart myportfolio
