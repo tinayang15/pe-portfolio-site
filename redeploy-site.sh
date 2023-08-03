638905dd2f0f381cc3702752c4ae6fb843d0cc98
Tina Yang <Tinayangers@gmail.com>
2023-08-03 01:40:33 -0500

    updated redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cd /root/pe-portfolio-site
+cd pe-portfolio-site
 
 git fetch && git reset origin/main --hard
 
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+docker compose -f docker-compose.prod.yml down
 
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml up -d --build
